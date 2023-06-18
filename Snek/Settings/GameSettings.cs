4bd9b89ac500d5a3ca2c6e1f3bdecd8397ad9b9d
devklick <devklick@gmail.com>
2023-06-18 11:30:42 +0100

    fix: GameSettings logging

--- Snek/Settings/GameSettings.cs
+++ Snek/Settings/GameSettings.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Snek.Settings;
 
@@ -74,12 +75,26 @@ public class GameSettings
 
     private string GetProp(Expression<Func<GameSettings, object>> exp)
     {
-        var name = ""; //...?
-        if (exp.Body is MemberExpression memberExpression)
+        var name = exp.Body switch
         {
-            name = memberExpression.Member.Name;
-        }
+            MemberExpression m => GetMemberName(m),
+            UnaryExpression u => GetMemberName(u),
+            _ => throw CannotGetMember(exp)
+        };
+
         var value = exp.Compile().Invoke(this);
         return $"{name}={value}";
     }
+
+    private static string GetMemberName(MemberExpression exp)
+        => exp.Member.Name;
+    private static string GetMemberName(UnaryExpression exp) => exp.Operand switch
+    {
+        MemberExpression m => GetMemberName(m),
+        _ => throw CannotGetMember(exp)
+    };
+
+    [DoesNotReturn]
+    private static Exception CannotGetMember(Expression exp)
+        => throw new NotImplementedException($"Cannot get member name from a ${exp.GetType().Name}");
 }
